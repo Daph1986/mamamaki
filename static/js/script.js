84018bbf3672f20a22eec51f0f19760f6338018f
Daph1986 <daphne.heimgartner@gmail.com>
2021-06-22 12:21:11 +0200

    Updated style and content single recipe page

--- static/js/script.js
+++ static/js/script.js
@@ -8,19 +8,3 @@ $(document).ready(function () {
   $('.collapsible').collapsible();
   $('input#japanese_recipe_name, input#english_recipe_name, textarea#recipe_introduction, input#recipe_preparation_time, input#recipe_servings, input#recipe_ingredients, textarea#recipe_instruction').characterCounter();
 });
-
-
-var coll = document.getElementsByClassName("collapsibles");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
